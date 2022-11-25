20e22fc3b2212fa42ed7c94bd96fc366f2a6d943
Andrei Nikonorov <nikorova@mail.ru>
2022-11-25 14:32:20 +0300

    not working thing

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/core/jdbc/util/HiberUtil.java
@@ -0,0 +1,57 @@
+package jm.core.jdbc.util;
+
+import jm.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class HiberUtil {
+    static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    static final String user = "postgres";
+    static final String password = "postgres";
+
+    public static SessionFactory buildSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+            Configuration configuration = new Configuration();
+
+            //Create Properties, can be read from property files too
+            Properties props = new Properties();
+            props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
+            props.put("hibernate.connection.url", url);
+            props.put("hibernate.connection.username", user);
+            props.put("hibernate.connection.password", password);
+            props.put("hibernate.hbm2ddl.auto", "create");
+            props.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            configuration.setProperties(props);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        return sessionFactory;
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("connection failed");
+        }
+        return connection;
+    }
+
+
+}
